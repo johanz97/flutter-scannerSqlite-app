0c5270378d50cf088d62275b55eb9931e5327820
johanz97 <ejzambrano4@espe.edu.ec>
2021-09-10 11:41:25 -0500

    Agregando Google Maps

--- lib/models/scan_model.dart
+++ lib/models/scan_model.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
 String scanModelToJson(ScanModel data) => json.encode(data.toJson());
@@ -18,6 +19,13 @@ class ScanModel {
   int? id;
   String? tipo;
   String valor;
+
+  LatLng getLatLng() {
+    final latLng = this.valor.substring(4).split(',');
+    final lat = double.parse(latLng[0]);
+    final lng = double.parse(latLng[1]);
+    return LatLng(lat, lng);
+  }
 
   factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
         id: json["id"],
--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,16 +1,58 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:scanner_sqlite/providers/db_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  MapType mapType = MapType.normal;
   @override
   Widget build(BuildContext context) {
     final scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
+    final CameraPosition puntoInicial =
+        CameraPosition(target: scan.getLatLng(), zoom: 17.5, tilt: 50);
+    Set<Marker> marker = new Set<Marker>();
+    marker.add(new Marker(
+        markerId: MarkerId('geo-location'), position: scan.getLatLng()));
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                final GoogleMapController controller = await _controller.future;
+                controller.animateCamera(CameraUpdate.newCameraPosition(
+                    CameraPosition(
+                        target: scan.getLatLng(), zoom: 17.5, tilt: 50)));
+              },
+              icon: Icon(
+                Icons.location_on,
+              ))
+        ],
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        mapType: mapType,
+        markers: marker,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.layers),
+        onPressed: () {
+          if (mapType == MapType.normal)
+            mapType = MapType.satellite;
+          else
+            mapType = MapType.normal;
+          setState(() {});
+        },
       ),
     );
   }
